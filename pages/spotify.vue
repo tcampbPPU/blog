bc6fd03398eb2b882d642b69d9e056b45641bc09
tanner <tanner.campbell@mile6.com>
2023-03-29 20:46:56 -0400

    add social links to about page

---
+++ pages/spotify.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  mounted () {
+    window.location.href = 'https://open.spotify.com/user/tc022'
+  },
+}
+</script>
+
+<template>
+  <div class="text-center dark:text-white">
+    You are being redirected to <a href="https://open.spotify.com">Spotify</a>
+  </div>
+</template>
